0044ddb55455e2c06c0faf10c42e71cf92ca551e
Nikhil <nikhilsamuel.89@gmail.com>
2018-07-30 17:25:36 -0400

    Create homepage and add links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
-//= require bootstrap-sprockets
+=* require jquery
+//= require bootstrap
 //= require activestorage
 //= require turbolinks
 //= require_tree .
